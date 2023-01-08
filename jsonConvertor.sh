b0c85afc2b6b5e7b7618388492a58885581d29b3
gokul <deangokul57@gmail.com>
2023-01-08 16:57:50 +0530

    git

--- jsonConvertor.sh
+++ jsonConvertor.sh
@@ -3,34 +3,34 @@
 x="`wget -qO- --no-check-certificate https://creatorapp.zohopublic.com/site24x7/location-manager/json/IP_Address_View/C80EnP71mW2fDd60GaDgnPbVwMS8AGmP85vrN27EZ1CnCjPwnm0zPB5EX4Ct4q9n3rUnUgYwgwX0BW3KFtxnBqHt60Sz1Pgntgru `"
 x=`echo $x | sed 's/}/\\n/g' `
 create_date="$(date +'%d/%m/%Y %T')" 
-echo "{\"created_date\":\"$create_date \","> awsJsonFormat.json
-echo "ip_prefix : [" >> awsJsonFormat.json
-n_id=1
+echo "{\"created_date\":\"$create_date \",">AwsJsonFormat.json
+echo "\"prefixes\" : [" >>AwsJsonFormat.json
+# n_id=1
 n_city=1
-n_place=1
-n_external_ip=1;
+# n_place=1
+n_external_ip=1
 for ip in ${x[@]};
 do
-    id=`echo $ip | sed 's/,/\\n/g' | grep "ID"|sed 's/\<ID\>//g'|tr -d ':""'`
+    # id=`echo $ip | sed 's/,/\\n/g' | grep "ID"|sed 's/\<ID\>//g'|tr -d ':""'`
     city=`echo $ip | sed 's/,/\\n/g' | grep "City"|sed 's/\<City\>//g'|tr -d ':""'`
-    place=`echo $ip | sed 's/,/\\n/g' | grep "Place"|sed 's/\<Place\>//g'|tr -d ':""'`
+    # place=`echo $ip | sed 's/,/\\n/g' | grep "Place"|sed 's/\<Place\>//g'|tr -d ':""'`
     external_ip=`echo $ip | sed 's/,/\\n/g' | grep "external_ip"|sed 's/\<external_ip\>//g'|tr -d ':""'`
     
-   if [[ ! -z $id  &&   $n_id == 1 ]];then
+    # if [[ ! -z $id  &&   $n_id == 1 ]];then
     
-        n_id=$id
+    #     n_id=$id
    
-    fi
+    # fi
     if [[ ! -z $city &&  $n_city -eq 1 ]];then
     
         n_city=$city
     
     fi
-    if [[ ! -z $place &&  $n_place -eq 1 ]];then
+    # if [[ ! -z $place &&  $n_place -eq 1 ]];then
    
-        n_place=$place
+    #     n_place=$place
     
-    fi
+    # fi
     if [[ ! -z $external_ip &&   $n_external_ip == 1 ]];then
 
         alreadyCidr=0
@@ -49,21 +49,52 @@ do
         fi
     
     if [[  $n_id != 1 &&  $n_city != 1 &&  $n_place != 1 &&   $n_external_ip != 1 ]];then
-        echo "{" >>awsJsonFormat.json
-        echo "\"id\":\"$n_id\",">> awsJsonFormat.json
-        echo "\"city\":\"$n_city\",">> awsJsonFormat.json
-        echo "\"place\":\"$n_place\",">> awsJsonFormat.json
-        echo "\"external_ip\":\"$n_external_ip\"">> awsJsonFormat.json
-        echo "}," >> awsJsonFormat.json
-        n_id=1
+        echo "{" >>aa.json
+        echo "\"ip_prefix\":\"$n_external_ip\",">>AwsJsonFormat.json
+        echo "\"region\":\"$n_city\",">>AwsJsonFormat.json
+        echo "\"service\":\"site24x7\",">>AwsJsonFormat.json
+        echo "\"network_border_group\":\"$n_city\"">>AwsJsonFormat.json
+       
+        echo "}," >>AwsJsonFormat.json
+        # n_id=1
         n_city=1
-        n_place=1
+        # n_place=1
         n_external_ip=1
       
     else
         continue
     fi 
 done
-echo "]  }" >> awsJsonFormat.json
+echo "],">>AwsJsonFormat.json
+echo "\"ipv6_prefixes\" : [{" >>AwsJsonFormat.json
+n_ipv6=1
+n_city=1
+for ip in ${x[@]};
+do 
+    ipv6=`echo $ip | sed 's/,/\\n/g' | grep "IPv6_Address_External"|sed 's/\<IPv6_Address_External\>//g'|tr -d '{""'|tr -d ':""'`
+    city=`echo $ip | sed 's/,/\\n/g' | grep "City"|sed 's/\<City\>//g'|tr -d ':""'`
+    if [[  ! -z $ipv6  && $ipv6 != 'IP_Address_View[' ]];then
+        if [[ ! -z $ipv6 &&  $n_ipv6 -eq 1 ]];then
+    
+            n_ipv6=$ipv6
+    
+        fi
+        if [[ ! -z $city &&  $n_city -eq 1 ]];then
+    
+            n_city=$city
+    
+        fi
+        if [[   $n_city != 1 &&    $n_ipv6 != 1 ]];then
+        echo "{" >>aa.json
+        echo "\"ipv6_prefix\":\"$n_ipv6\",">>AwsJsonFormat.json
+        echo "\"region\":\"$n_city\",">>AwsJsonFormat.json
+        echo "\"service\":\"site24x7\",">>AwsJsonFormat.json
+        echo "\"network_border_group\":\"$n_city\"">>AwsJsonFormat.json
+        echo "}," >>AwsJsonFormat.json
+        n_ipv6=1
+        n_city=1
+        fi
+    fi
+done
 
-
+echo "] }" >>AwsJsonFormat.json
